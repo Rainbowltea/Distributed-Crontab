70807c7087d858f23069f747e835cb30399da517
XiangXuhuai <1506347180@qq.com>
2022-10-27 22:24:15 +0800

    解析命令行参数并启动任务管理器

--- master/main/master.go
+++ master/main/master.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"Ditributed-Crontab/master"
+	"flag"
+	"fmt"
+)
+
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// master -config ./master.json -xxx 123 -yyy ddd
+	// master -h
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json")
+	flag.Parse()
+}
+
+func main() {
+	var (
+		err error
+	)
+	//加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+	//启动任务管理器
+	if err = master.InitJobMgr(); err != nil {
+		goto ERR
+	}
+	//启动API HTTP服务
+	if err = master.InitApiServer(); err != nil {
+		goto ERR
+	}
+ERR:
+	fmt.Println(err)
+}
