a6534de911b43acff174d6b418b7f6d6807cc277
XiangXuhuai <1506347180@qq.com>
2022-10-29 19:57:07 +0800

    定义任务调度事件

--- common/Protocol.go
+++ common/Protocol.go
@@ -1,6 +1,12 @@
 package common
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/gorhill/cronexpr"
+)
 
 // 定时任务
 type Job struct {
@@ -16,6 +22,19 @@ type Response struct {
 	Data  interface{} `json:"data"`
 }
 
+//任务调度计划
+type JobSchedulePlan struct {
+	Job      *Job
+	Expr     *cronexpr.Expression //解析好的cronexpr表达式
+	NextTime time.Time            //下次调度的时间
+}
+
+// 变化事件
+type JobEvent struct {
+	EventType int //  SAVE, DELETE
+	Job       *Job
+}
+
 // 应答方法
 func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err error) {
 	// 1, 定义一个response
@@ -29,6 +48,61 @@ func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err er
 
 	// 2, 序列化json
 	resp, err = json.Marshal(response)
+	return
+
+}
+
+// 反序列化Job
+func UnpackJob(value []byte) (ret *Job, err error) {
+	var (
+		job *Job
+	)
+
+	job = &Job{}
+	if err = json.Unmarshal(value, job); err != nil {
+		return
+	}
+	ret = job
+	return
+
+}
+
+// 从etcd的key中提取任务名
+// /cron/jobs/job10抹掉/cron/jobs/
+func ExtractJobName(jobKey string) string {
+	return strings.TrimPrefix(jobKey, JOB_SAVE_DIR)
+}
+
+// 从 /cron/killer/job10提取job10
+func ExtractKillerName(killerKey string) string {
+	return strings.TrimPrefix(killerKey, JOB_KILLER_DIR)
+}
+
+// 任务变化事件有2种：1）更新任务 2）删除任务
+func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
+	return &JobEvent{
+		EventType: eventType,
+		Job:       job,
+	}
+}
+
+// 构造任务执行计划
+func BuildJobSchedulePlan(job *Job) (jobSchedulePlan *JobSchedulePlan, err error) {
+	var (
+		expr *cronexpr.Expression
+	)
+
+	// 解析JOB的cron表达式
+	if expr, err = cronexpr.Parse(job.CronExpr); err != nil {
+		return
+	}
+
+	// 生成任务调度计划对象
+	jobSchedulePlan = &JobSchedulePlan{
+		Job:      job,
+		Expr:     expr,
+		NextTime: expr.Next(time.Now()),
+	}
 	return
 
 }
