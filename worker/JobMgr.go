a3dc562ae4892d78c5e2c757390ea85f6f0c77bf
XiangXuhuai <1506347180@qq.com>
2022-10-29 16:34:14 +0800

    实现监听任务变化功能

---
+++ worker/JobMgr.go
@@ -0,0 +1,117 @@
+package worker
+
+import (
+	"Ditributed-Crontab/common"
+	"context"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+)
+
+// 任务管理器
+type JobMgr struct {
+	client  *clientv3.Client
+	kv      clientv3.KV
+	lease   clientv3.Lease
+	watcher clientv3.Watcher
+}
+
+var (
+	// 单例
+	G_jobMgr *JobMgr
+)
+
+// 初始化管理器
+func InitJobMgr() (err error) {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		kv      clientv3.KV
+		lease   clientv3.Lease
+		watcher clientv3.Watcher
+	)
+
+	// 初始化配置
+	config = clientv3.Config{
+		Endpoints:   G_config.EtcdEndpoints,                                     // 集群地址
+		DialTimeout: time.Duration(G_config.EtcdDialTimeout) * time.Millisecond, // 连接超时
+	}
+
+	// 建立连接
+	if client, err = clientv3.New(config); err != nil {
+		return
+	}
+
+	// 得到KV和Lease的API子集
+	kv = clientv3.NewKV(client)
+	lease = clientv3.NewLease(client)
+	watcher = clientv3.NewWatcher(client)
+
+	// 赋值单例
+	G_jobMgr = &JobMgr{
+		client:  client,
+		kv:      kv,
+		lease:   lease,
+		watcher: watcher,
+	}
+
+	// 启动任务监听
+	G_jobMgr.watchJobs()
+	return
+
+}
+
+// 监听任务变化
+func (jobMgr *JobMgr) watchJobs() (err error) {
+	var (
+		getResp            *clientv3.GetResponse
+		job                *common.Job
+		watchStartRevision int64
+		watchChan          clientv3.WatchChan
+		watchResp          clientv3.WatchResponse
+		watchEvent         *clientv3.Event
+		jobName            string
+		jobEvent           *common.JobEvent
+	)
+
+	// 1, get一下/cron/jobs/目录下的所有任务，并且获知当前集群的revision
+	if getResp, err = jobMgr.kv.Get(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithPrefix()); err != nil {
+		return
+	}
+	// 当前有哪些任务
+	for _, kvpair := range getResp.Kvs {
+		// 反序列化json得到Job
+		if job, err = common.UnpackJob(kvpair.Value); err == nil {
+			//TODO: 同步给scheduler(调度协程)
+		}
+	}
+	//从该Version后监听变化事件
+	go func() {
+		// 从GET时刻的后续版本开始监听变化
+		watchStartRevision = getResp.Header.Revision + 1
+		// 监听/cron/jobs/目录的后续变化
+		watchChan = jobMgr.watcher.Watch(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithRev(watchStartRevision))
+		// 处理监听事件
+		for watchResp = range watchChan {
+			for _, watchEvent = range watchResp.Events {
+				switch watchEvent.Type {
+				case mvccpb.PUT:
+					if job, err = common.UnpackJob(watchEvent.Kv.Value); err != nil {
+						continue
+					}
+					//构建一个更新Event
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
+					//TODO:反序列化job，推送给scheduler
+
+				case mvccpb.DELETE:
+					//提取任务名
+					jobName = common.ExtractJobName(string(watchEvent.Kv.Key))
+					//构建一个删除Event
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_DELETE, job)
+					//TODO:推送一个删除事件给schel
+				}
+			}
+		}
+	}()
+}
