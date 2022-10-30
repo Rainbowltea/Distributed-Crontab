216d7abf6aa94c5cf0d1b847d04fd9b0ad7c60fe
XiangXuhuai <1506347180@qq.com>
2022-10-30 15:32:01 +0800

    实现任务调度器

--- worker/Scheduler.go
+++ worker/Scheduler.go
@@ -1,6 +1,9 @@
 package worker
 
-import "Distributed-Crontab/common"
+import (
+	"Distributed-Crontab/common"
+	"time"
+)
 
 //任务调度
 type Scheduler struct {
@@ -32,17 +35,64 @@ func (scheduler *Scheduler) handleJobEvent(jobEvent *common.JobEvent) {
 	}
 }
 
+//重新计算任务调度状态
+func (scheduler *Scheduler) TrySchedule() (scheduleAfter time.Duration) {
+	var (
+		jobPlan  *common.JobSchedulePlan
+		now      time.Time
+		nearTime *time.Time
+	)
+	// 如果任务表为空话，随便睡眠多久
+	if len(scheduler.jobPlanTable) == 0 {
+		scheduleAfter = 1 * time.Second
+		return
+	}
+
+	//获取当前时间
+	now = time.Now()
+	//1遍历所有任务
+	for _, jobPlan = range scheduler.jobPlanTable {
+		if jobPlan.NextTime.Before(now) || jobPlan.NextTime.Equal(now) {
+			//TODO尝试执行任务，因为可能前一个任务还没有执行完
+			jobPlan.NextTime = jobPlan.Expr.Next(now) // 更新下次执行时间
+		}
+
+		//统计一个最近要过期的任务时间
+		if nearTime == nil || jobPlan.NextTime.Before(*nearTime) {
+			nearTime = &jobPlan.NextTime
+		}
+	}
+	//下次调度间隔
+	scheduleAfter = (*nearTime).Sub(now)
+	return
+	//2过期的任务立即执行
+	//3统计最近的要过期的任务的时间（N秒后过期==scheduleAfter）
+
+}
+
 //调度协程
 func (scheduler *Scheduler) scheduleLoop() {
 	var (
-		jobEvent *common.JobEvent
+		jobEvent      *common.JobEvent
+		scheduleAfter time.Duration
+		scheduleTimer *time.Timer
 	)
+	//初始化一次(1s)
+	scheduleAfter = scheduler.TrySchedule()
+
+	//调度的延迟定时器
+	scheduleTimer = time.NewTimer(scheduleAfter)
 	for {
 		select {
 		case jobEvent = <-scheduler.jobEventChan: //监听任务变化时间
 			//对内存中维护的任务列表做增删改查,和etcd中的任务保持一致
 			scheduler.handleJobEvent(jobEvent)
+		case <-scheduleTimer.C: //最近的任务到期了
 		}
+		//重新调度一次任务
+		scheduleAfter = scheduler.TrySchedule()
+		//重置调度间隔
+		scheduleTimer.Reset(scheduleAfter)
 	}
 }
 
