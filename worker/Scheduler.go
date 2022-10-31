0383cf2e1bfe198d6337e98528159f55a60cddea
XiangXuhuai <1506347180@qq.com>
2022-10-31 17:15:49 +0800

    实现尝试执行任务功能

--- worker/Scheduler.go
+++ worker/Scheduler.go
@@ -2,13 +2,15 @@ package worker
 
 import (
 	"Distributed-Crontab/common"
+	"fmt"
 	"time"
 )
 
 //任务调度
 type Scheduler struct {
-	jobEventChan chan *common.JobEvent              //etcd任务事件队列
-	jobPlanTable map[string]*common.JobSchedulePlan //任务调度计划表
+	jobEventChan      chan *common.JobEvent              //etcd任务事件队列
+	jobPlanTable      map[string]*common.JobSchedulePlan //任务调度计划表
+	jobExecutingTable map[string]*common.JobExecuteInfo  // 任务执行表
 }
 
 var (
@@ -54,6 +56,7 @@ func (scheduler *Scheduler) TrySchedule() (scheduleAfter time.Duration) {
 	for _, jobPlan = range scheduler.jobPlanTable {
 		if jobPlan.NextTime.Before(now) || jobPlan.NextTime.Equal(now) {
 			//TODO尝试执行任务，因为可能前一个任务还没有执行完
+			scheduler.TryStartJob(jobPlan)
 			jobPlan.NextTime = jobPlan.Expr.Next(now) // 更新下次执行时间
 		}
 
@@ -68,6 +71,29 @@ func (scheduler *Scheduler) TrySchedule() (scheduleAfter time.Duration) {
 	//2过期的任务立即执行
 	//3统计最近的要过期的任务的时间（N秒后过期==scheduleAfter）
 
+}
+
+// 尝试执行任务
+func (scheduler *Scheduler) TryStartJob(jobPlan *common.JobSchedulePlan) {
+	//调度和执行是两件事情
+	//执行的任务可能会执行很久,1分钟会调度60此，但是只能执行一次，防止并发
+	var (
+		jobExecuteInfo *common.JobExecuteInfo
+		jobExecuting   bool //判断任务是否正在执行
+	)
+	//如果任务正在执行，跳过本次任务调度
+	if jobExecuteInfo, jojobExecuting = scheduler.jobExecutingTable[jobPlan.Job.Name]; jobExecuting {
+		fmt.Println("尚未退出，跳过执行")
+		return
+	}
+	//构建执行状态信息
+	jobExecuteInfo = common.BUildExecuteInfo(jobPlan)
+
+	//保存执行状态
+	scheduler.jobExecutingTable[jobPlan.Job.Name] = jobExecuteInfo
+
+	//执行任务
+	//TODO:并发执行任务
 }
 
 //调度协程
@@ -104,8 +130,9 @@ func (scheduler *Scheduler) PushJobEvent(jobEvent *common.JobEvent) {
 //初始化调度器
 func InitScheduler() (err error) {
 	G_scheduler = &Scheduler{
-		jobEventChan: make(chan *common.JobEvent, 1000),
-		jobPlanTable: make(map[string]*common.JobSchedulePlan),
+		jobEventChan:      make(chan *common.JobEvent, 1000),
+		jobPlanTable:      make(map[string]*common.JobSchedulePlan),
+		jobExecutingTable: make(map[string]*common.JobExecuteInfo),
 	}
 	//启动调度协程
 	go G_scheduler.scheduleLoop()
