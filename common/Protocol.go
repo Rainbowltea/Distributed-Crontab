227545fb108a485efedd28c79567b2be7a7bfa09
XiangXuhuai <1506347180@qq.com>
2022-11-19 20:52:19 +0800

    日志过滤条件

--- common/Protocol.go
+++ common/Protocol.go
@@ -34,6 +34,16 @@ type LogBatch struct {
 	Logs []interface{} // 多条日志
 }
 
+// 任务日志过滤条件
+type JobLogFilter struct {
+	JobName string `bson:"jobName"`
+}
+
+// 任务日志排序规则
+type SortLogByStartTime struct {
+	SortOrder int `bson:"startTime"` // {startTime: -1}
+}
+
 // HTTP接口应答
 type Response struct {
 	Errno int         `json:"errno"`
